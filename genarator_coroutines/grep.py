d27bddc7a15eea58fce90e49584c144fcfa09859
m-bkarimi <bacspython@gmail.com>
2022-01-21 23:10:19 +0330

    Add coroutine decarator

--- genarator_coroutines/grep.py
+++ genarator_coroutines/grep.py
@@ -1,11 +1,17 @@
 # Coroutines
 # call generator with parametr..
+def coroutine(func):
+    def start(*args,**kwargs):
+        cr = func(*args,**kwargs)
+        next(cr, 'default') # must be call befor call send method...
+        return cr
+    return start
+@coroutine
 def grep(pattrn):
     while True:
-        print('ok')
         line = yield
         if pattrn in line:
             print(line)
 g = grep("python")
-next(g, 'default') # must be call befor call send method...
+#next(g, 'default') # must be call befor call send method...
 g.send("python Yeah, but no, but yeah, but no")
